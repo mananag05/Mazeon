bcfa84bfd556dad2ca12df92dde10b0f7359fcc6
Manan Agarwal <maverickmanan@gmail.com>
2024-04-04 17:33:18 +0530

    grid makin

--- client/src/components/canvas/index.js
+++ client/src/components/canvas/index.js
@@ -1,16 +1,30 @@
-"use client"
-
-import React from "react";
-import { Stage, Layer, Rect } from "react-konva";
+import React, { useState } from "react";
 
 const PlayGround = () => {
+  const [rowGrid, setrowGrid] = useState(12);
+  const [rowCols , setRowCols] = useState(15);
+  const [girdMappin , setrowGridMappin] = useState([
+    
+  ])
+  
+
+
+
   return (
-    <div>
-      <Stage width={500} height={200}>
-        <Layer>
-          <Rect x={20} y={20} width={50} height={50} fill="red" draggable />
-        </Layer>
-      </Stage>
+    <div className="flex flex-col items-center mt-10">
+      {Array.from({ length: rowGrid }, (_, index) => (
+        <div
+          key={index}
+          className={`flex flex-row bg-logthemstext`}
+          style={{ width: `${rowCols}rem`, height: '1rem' }}
+        >
+          {Array.from({ length : rowCols} , (_,index) => (
+                <div className="bg-red" style={{ width : `1rem` , hight: '1rem'}}>
+
+                </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
